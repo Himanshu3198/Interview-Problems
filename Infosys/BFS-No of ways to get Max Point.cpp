53e31a9e0b65075b2a0367c94a1f9a50893056e5
HIMANSHU SHARMA <54854216+Himanshu3198@users.noreply.github.com>
2022-01-28 18:23:11 +0530

    Update BFS-No of ways to get Max Point.cpp

--- Infosys/BFS-No of ways to get Max Point.cpp
+++ Infosys/BFS-No of ways to get Max Point.cpp
@@ -1,4 +1,39 @@
 
+/* 
+
+Rahal the magician was making a new magical armor . She went to the Twilight city to get some magical points for her armor
+Twlight city has N+1 magical houses indexded from 1 to N+1 . At each house from 1 to N theres a fairy that will give
+Rahan A[i] (1<=i<=N) magical points.
+
+it is given that A[i] might be negative  which means that the fairy took some magical points from Rahaf . Additonally at each house
+from 1 to N there's a teleporter with power p[i] that Rahaf can use to go from house index i to any house with an index from
+i+1 to i+p[i]  at the first Rahaf is in the first house and she wants to go the (N+1)th house with maximum number of magical points .
+
+Your task is to find the total number of ways to go from first house to the (N+1)th house with maximum number of points . 
+since answer might be very large  you need to return it modulo 10^9+7 .
+    
+    constraints
+    1<=N<=10^5
+    -10^9<=A[i]<=10^9
+    1<=p[i]<=N
+    
+    test cases:
+   input:
+     2
+    3 
+    0 0 0
+    3 1 1
+    4
+    3 1 -1 4 
+    4 1 2  1
+         
+   ouput:
+         3
+         2
+            
+             
+*/     
+
 // himanshu3198
 #include<bits/stdc++.h>
 #define ll long long int 
